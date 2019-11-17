317d873ff773a8ff7583599d6dd4565e3b05c488
Alain Garcia <alaingarcia@ufl.edu>
2019-11-17 16:06:55 -0500

    Added accuracy score, timing

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 from CA import CA
 from EM import EM
 import pandas as pd
+import time
 
 
 if __name__ == '__main__':
@@ -8,10 +9,31 @@ if __name__ == '__main__':
     InCenters = pd.read_csv('data/InCenters.csv', header=None)
     InData = pd.read_csv('data/InData.csv', header=None)
 
-    # Get results
+    # Get results and timing
+    CA_start_time = time.time_ns()
     NumClust, OutCenter, Classifications = CA.CA(InData, InCenters, MaximumIt=50)
+    CA_end_time = time.time_ns()
+
+    # Print results
     print("CA final number of cluster: {}".format(NumClust))
     print("CA cluster centers: {}".format(OutCenter))
     print("CA classification vector: {}".format(Classifications))
+    
+    # Convert ns to ms
+    CA_time = (CA_end_time-CA_start_time)/1e6
 
-    ExMa = EM.ExpectationMaximization(InData, InCenters, rep=50)
+    #ExMa = EM.ExpectationMaximization(InData, InCenters, rep=50)
+
+    def accuracy(actual_classification_vector, attempt_classification_vector):
+        correct = 0
+        for i in range(0, len(actual_classification_vector)):
+            if actual_classification_vector[i] == attempt_classification_vector[i]:
+                correct += 1
+        return correct/len(actual_classification_vector)
+
+    actual_classification_file = pd.read_csv("data/ActualClassification.csv", header=None)
+    actual_classification_vector = actual_classification_file[0].tolist()
+    CA_accuracy = accuracy(actual_classification_vector,Classifications)
+
+    print("CA time: {} ms".format(CA_time))
+    print("CA accuracy: {}%".format(CA_accuracy*100))
