14112f23e06128c21534ed7923312f1cf3c42660
Alain Garcia <alaingarcia@ufl.edu>
2019-11-25 23:52:16 -0500

    added indeces for training

--- python/data/training/training.py
+++ python/data/training/training.py
@@ -7,6 +7,7 @@ classifications = pd.read_csv("a1-ga.csv", names=["cluster"])
 
 data_with_labels = pd.concat([data_points, classifications], axis=1)
 smallest_points = pd.DataFrame()
+index_series = pd.Series()
 
 for index, row in cluster_centers.iterrows():
     distances = ((data_points["x"]-row["x"]).pow(2) + (data_points["y"]-row["y"]).pow(2)).pow(0.5)
@@ -15,9 +16,11 @@ for index, row in cluster_centers.iterrows():
     y = data_points["y"].iloc[indeces]
     temp_smallest = pd.concat([x,y], axis=1)
     smallest_points = smallest_points.append(temp_smallest)
+    index_series = index_series.append(pd.Series(indeces))
 
     # DEBUG PRINTS
     #print("Closest point to ({},{}) is".format(row["x"], row["y"]))
     #print(temp_smallest)
 
 smallest_points.to_csv("training.csv", index=False, header=None)
+index_series.to_csv("training_index.csv", index=False, header=None)
